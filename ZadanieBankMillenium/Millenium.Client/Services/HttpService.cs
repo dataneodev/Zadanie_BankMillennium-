9be57e4157f0f0ec4cbf2a14822a175e53899b9e
Grzegorz Kapustyński <gkapustynski@vanto.pl>
2025-08-06 16:05:26 +0200

    [0]Update

---
+++ ZadanieBankMillenium/Millenium.Client/Services/HttpService.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+
+namespace Millenium.Client;
+
+internal sealed class HttpService : IHttpService
+{
+    private const string RestKey = "RestKey";
+    private readonly IDistributedCache _cache;
+
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public HttpService(IHttpClientFactory httpClientFactory,
+        IDistributedCache cache)
+    {
+        _httpClientFactory = httpClientFactory;
+        _cache = cache;
+    }
+
+    public async Task<string?> GetRestData()
+    {
+        var cached = await _cache.GetStringAsync(RestKey);
+        if (cached != null) return cached;
+
+        var client = _httpClientFactory.CreateClient();
+        var response = await client.GetAsync("https://localhost:7161/fake");
+        response.EnsureSuccessStatusCode();
+
+        var result = await response.Content.ReadAsStringAsync();
+
+        var cacheOptions = new DistributedCacheEntryOptions()
+            .SetSlidingExpiration(TimeSpan.FromMinutes(5));
+
+        await _cache.SetStringAsync(RestKey, result, cacheOptions);
+
+        return result;
+    }
+}
---
+++ ZadanieBankMillenium/Millenium.Client/Services/IHttpService.cs
@@ -0,0 +1,6 @@
+﻿namespace Millenium.Client.Services;
+
+public interface IHttpService
+{
+    Task<string?> GetRestData();
+}
